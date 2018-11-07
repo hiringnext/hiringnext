32124eeb0ae54a08710e893f30c6ff6104f334d5
hiringnext <contact.vishal22@gmail.com>
2018-11-07 17:36:50 +0530

    Update requirements.txt

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -7,12 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
-
 from whitenoise.django import DjangoWhiteNoise
 
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
