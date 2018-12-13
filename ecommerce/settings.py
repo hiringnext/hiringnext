2ed82a3a03c5bc5dbf047f012db341bd1dee2540
hiringnext <contact.vishal22@gmail.com>
2018-12-13 22:23:44 +0530

    update newsletter

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -34,7 +34,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'automail.empower@gmail.com'
-EMAIL_HOST_PASSWORD = 'vishal@2288'
+EMAIL_HOST_PASSWORD = 'Empower$2288'
 
 # Application definition
 
